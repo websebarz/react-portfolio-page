60ac79feb501b321cdee5e628fd4f87ebac145dd
Seb Arz <109311254+websebarz@users.noreply.github.com>
2022-08-29 12:28:06 -0500

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import "./App.css";
+import "./index.css";
+import Counter from "./comps/Counter";
+
+function App() {
+  return (
+    <div className="App">
+      <Counter />
+    </div>
+  );
+}
+
+export default App;
